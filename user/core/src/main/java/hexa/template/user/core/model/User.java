60a3c34ad089ffc4d177d1aabe0473b0728c367c
PETIT LOIC <externe.loic.petit@agipi.com>
2026-02-27 09:47:47 +0100

    User core init module add user model

---
+++ user/core/src/main/java/hexa/template/user/core/model/User.java
@@ -0,0 +1,30 @@
+package hexa.template.user.core.model;
+
+import lombok.Builder;
+
+import java.util.Optional;
+
+@Builder
+public record User (
+    Long id,
+    String firstName,
+    String name,
+    Long emailId
+) {
+    public User {
+        if (firstName == null || firstName.trim().isEmpty()) {
+            throw new IllegalArgumentException("the user first name cannot be blank");
+        }
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("the user name cannot be blank");
+        }
+    }
+
+    public Optional<Long> ifId() {
+        return Optional.ofNullable(id);
+    }
+
+    public Optional<Long> ifEmailId() {
+        return Optional.ofNullable(emailId);
+    }
+}
---
+++ user/core/src/test/java/hexa/template/user/core/model/UserTest.java
@@ -0,0 +1,101 @@
+package hexa.template.user.core.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class UserTest {
+    @Test
+    void mustCreateUser() {
+        final var user = User.builder()
+                .id(1L)
+                .firstName("Chuck")
+                .name("Norris")
+                .emailId(2L)
+                .build();
+
+        assertThat(user)
+                .as("user")
+                .isNotNull()
+                .satisfies(
+                    u -> assertThat(u.id())
+                            .as("id")
+                            .isEqualTo(1L),
+                    u -> assertThat(u.ifId())
+                            .as("if id")
+                            .isPresent()
+                            .get()
+                            .isEqualTo(1L),
+                    u -> assertThat(u.firstName())
+                            .as("first name")
+                            .isEqualTo("Chuck"),
+                    u -> assertThat(u.name())
+                            .as("name")
+                            .isEqualTo("Norris"),
+                    u -> assertThat(u.emailId())
+                            .as("email id")
+                            .isEqualTo(2L),
+                    u -> assertThat(u.ifEmailId())
+                            .as("if email id")
+                            .isPresent()
+                            .get()
+                            .isEqualTo(2L)
+                );
+    }
+
+    @Test
+    void ifOptionalFieldsNullMustCreateUser() {
+        final var user = User.builder()
+                .firstName("Chuck")
+                .name("Norris")
+                .build();
+
+        assertThat(user)
+                .as("user")
+                .isNotNull()
+                .satisfies(
+                    u -> assertThat(u.id())
+                            .as("id")
+                            .isNull(),
+                    u -> assertThat(u.ifId())
+                            .as("if id")
+                            .isEmpty(),
+                    u -> assertThat(u.emailId())
+                            .as("email id")
+                            .isNull(),
+                    u -> assertThat(u.ifEmailId())
+                            .as("if email id")
+                            .isEmpty()
+                );
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = "  ")
+    void ifFirstNameBlankMustThrowException(final String firstName) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> User.builder()
+                    .firstName(firstName)
+                    .name("Norris")
+                    .build()
+                )
+                .withMessage("the user first name cannot be blank");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = "  ")
+    void ifNameBlankMustThrowException(final String name) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> User.builder()
+                    .firstName("Chuck")
+                    .name(name)
+                    .build()
+                )
+                .withMessage("the user name cannot be blank");
+    }
+}
